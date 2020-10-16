9c483f91ee8220aa2fc71afc9ccb013b2334000f
Axel Roville <a.roville@criteo.com>
2020-10-16 20:51:50 +0200

    Fix NPE in UI, print link to logs and drilldown

--- service/src/java/org/apache/hive/service/cli/operation/LogDivertAppender.java
+++ service/src/java/org/apache/hive/service/cli/operation/LogDivertAppender.java
@@ -71,7 +71,8 @@ public class LogDivertAppender extends WriterAppender {
     private static final Pattern executionIncludeNamePattern = Pattern.compile(Joiner.on("|").
       join(new String[] {"org.apache.hadoop.mapreduce.JobSubmitter",
       "org.apache.hadoop.mapreduce.Job", "SessionState", Task.class.getName(),
-      Driver.class.getName(), "org.apache.hadoop.hive.ql.exec.spark.status.SparkJobMonitor"}));
+      Driver.class.getName(), "org.apache.hadoop.hive.ql.exec.spark.status.SparkJobMonitor",
+      "org.apache.hive.service.cli.operation.Operation"}));
 
     /* Patterns that are included in performance logging level.
      * In performance mode, show execution and performance logger messages.
